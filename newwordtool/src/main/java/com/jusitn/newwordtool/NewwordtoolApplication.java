d7e5772477f34622ffbbb70e5b1bd0e120e5be78
JustinZhang <zhangjunqiang7183@126.com>
2022-12-02 17:12:19 +0800

    init

---
+++ newwordtool/src/main/java/com/jusitn/newwordtool/NewwordtoolApplication.java
@@ -0,0 +1,13 @@
+package com.jusitn.newwordtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewwordtoolApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NewwordtoolApplication.class, args);
+  }
+
+}
---
+++ newwordtool/src/test/java/com/jusitn/newwordtool/NewwordtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jusitn.newwordtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewwordtoolApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
